697c6677984862f496e1190dc03bb252b3ad9257
sam <samuel.j.simmons@gmail.com>
2016-05-06 11:42:34 +1200

    ready to pull latest

--- src/js/components/pages/feedcontainer.js
+++ src/js/components/pages/feedcontainer.js
@@ -6,22 +6,13 @@ import Feed  from '.././feed'
 class FeedContainer extends Component{
 
  render(){
-<<<<<<< HEAD:src/js/components/feedcontainer.js
-  //  console.log('props in feed container',this.props)
-=======
->>>>>>> b45ed316a15c1554a82fd057f6beae77b7f18ff7:src/js/components/pages/feedcontainer.js
    const { feeds } = this.props
    let content
    feeds.length>0 ? content=feeds.map(feed => <Feed key={feed.id} {...feed}/>)
    : content=<h1>Loading</h1> ;
    return (
-<<<<<<< HEAD:src/js/components/feedcontainer.js
-     <div className="feed-container">
-        {feeds.slice(0,4).map(feed => <Feed key={feed.id} {...feed}/>)}
-=======
-     <div>
+     <div class="feed-container">
       {content}
->>>>>>> b45ed316a15c1554a82fd057f6beae77b7f18ff7:src/js/components/pages/feedcontainer.js
      </div>
    )
  }
