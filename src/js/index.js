ad0d8e35aacc418549a49370916d4e16278625fc
Vicken <vicken.liu@gmail.com>
2016-05-05 15:14:39 +1200

    set up the routes

--- src/js/index.js
+++ src/js/index.js
@@ -1,14 +1,39 @@
-import React,{Component} from 'react'
-import {render} from 'react-dom'
+//import the libs
+import React,{Component}       from 'react'
+import {render}                from 'react-dom'
+import { Provider }            from 'react-redux'
+import { Router, Route, IndexRoute, browserHistory } from "react-router";
+import { syncHistoryWithStore} from 'react-router-redux'
+import { Provider }            from 'react-redux'
+import { createStore }         from 'redux'
+//import the reducer
+import reducer        from './reducers'
+//import the components
+import Layout         from './components/layout'
+import Feedcontainer  from './components/feedcontainer'
+import Imagepage      from './components/imagepage'
+import Profile        from './components/profile'
+import Location       from './components/location'
+
+const store = createStore(reducer)
+const history = syncHistoryWithStore(browserHistory, store)
 
 
 class App extends Component{
  render(){
    return (
-     <h1>Hello {this.props.name}</h1>
+     <Provider store={store}>
+       <Router history={history}>
+         <Route path="/" component={Layout}>
+           <IndexRoute component={Feedcontainer}></IndexRoute>
+           <Route path="photo/:id" component={Imagepage}></Route>
+           <Route path="profile" component={Profile}></Route>
+           <Route path='location' component={Location}></Route>
+         </Route>
+       </Router>
+     </Provider>
    )
  }
 }
-
 
 render(<App name='Ollie'/>, document.getElementById('app'))
