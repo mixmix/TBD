cc5fa0a7234a655dd2999a425655283f56b8ed11
sam <samuel.j.simmons@gmail.com>
2016-05-07 00:15:49 +1200

    search function working, if matched hard

--- src/js/reducers/index.js
+++ src/js/reducers/index.js
@@ -4,9 +4,11 @@ import { routerReducer } from 'react-router-redux'
 
 import feeds from './feeds'
 import user from './user'
+import filter from './filter'
 
 export default combineReducers({
   feeds,
   user,
-  routing: routerReducer
+  routing: routerReducer,
+  filter
 })
