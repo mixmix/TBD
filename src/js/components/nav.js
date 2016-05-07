2ff0e24169e7d3eadc5c2505e6ba6e90643b48ea
Vicken <vicken.liu@gmail.com>
2016-05-08 10:34:16 +1200

    add smart nav

--- src/js/components/nav.js
+++ src/js/components/nav.js
@@ -1,22 +1,35 @@
 import React,{Component} from 'react'
-import { Link } from "react-router";
+import { Link }          from "react-router";
+import {connect}         from 'react-redux'
 
-export default class Nav extends Component{
+class Nav extends Component{
  render(){
-   const {location}=this.props
+   const {location,user}=this.props
    let indexActive=location.pathname==='/'? 'active' : ''
    let profileActive=location.pathname.match(/^\/profile/)? 'active' : ''
    let locationActive=location.pathname.match(/^\/location/)? 'active' : ''
+   const customizedNav= user.name!=='visitor'?
+   [<Link to='upload'>Upload</Link>,
+   <Link to='profile' class={profileActive}>profile</Link>] :
+   <Link to='login'>login</Link>
    return (
      <nav>
       <Link to='/' class={indexActive}>feeds</Link>
-      <Link to='profile' class={profileActive}>profile</Link>
       <Link to='location' class={locationActive}>location</Link>
-      <Link to='login'>login</Link>
-      <Link to='upload'>Upload</Link>
+      {customizedNav}
      </nav>
    )
  }
 }
 
 export {Nav}
+
+const mapStatetoProps = (state) => {
+  return {
+    user: state.user
+  }
+}
+
+export default connect(
+  mapStatetoProps
+)(Nav)
