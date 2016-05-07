b149b4c2aec9f24a6cd331cecdb2d4be4b7a1dff
Vicken <vicken.liu@gmail.com>
2016-05-07 18:42:18 +1200

    pass the reducers test

--- test/frontEnd/reducerUnit.js
+++
@@ -1,6 +0,0 @@
-import categories from '../../src/js/reducers/categories'
-import feeds      from '../../src/js/reducers/feeds'
-import user       from '../../src/js/reducers/user'
-
-import {expect} from 'chai'
-let assert = require('chai').assert;
